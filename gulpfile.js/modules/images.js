46af9af97da38fb6cb59ed8a274aa0a82aba4d0b
pengzai-dev <pengzai.dev@outlook.com>
2020-03-30 15:01:46 +0800

    优化script和image

--- gulpfile.js/modules/images.js
+++ gulpfile.js/modules/images.js
@@ -3,6 +3,7 @@ const config = require("../config");
 const fse = require("fs-extra");
 const tinypng = require("tinypngjs");
 const { src } = require("gulp");
+const glob = require("glob");
 const through = require("through2");
 const path = require("path");
 const low = require("lowdb");
@@ -16,52 +17,53 @@ const adapter = new FileSync(path.resolve(__dirname, "../cache/db.json"));
 const db = low(adapter);
 db.defaults({ images: [] }).write();
 const reload = require("./server").reload;
-// const readline = require("readline");
 const ProgressBar = require("../libs/progress");
+
 var progressBar = new ProgressBar();
 function images(cb2) {
 	let total = 0;
 	let loaded = 0;
-	src(`${config.src}**/*.{png,jpg,gif,ico,svg}`).pipe(
-		through.obj(function (file, enc, cb) {
-			cb();
-			total++;
-			let outPath = path.resolve(file.base, "../", config.dist, path.dirname(Utli.toVersionUrl(file.relative)), path.basename(file.relative));
-			function checkLoaded() {
-				function isEnd() {
-					loaded++;
-					progressBar.render({
-						description: "图片压缩",
-						completed: loaded,
-						total: total
-					});
-				
-					if (loaded >= total) {
-						reload();
-						cb2();
-					}
-				}
-				fse.copy(file.path, outPath)
-					.then(res => {
-						isEnd();
-					})
-					.catch(() => {
-						isEnd();
-					});
-			}
-			var stat = fse.statSync(file.path);
-			var relativePath = file.relative.replace(/\\/g, '/');
+	function isEnd() {
+		loaded++;
+		progressBar.render({
+			description: "图片压缩",
+			completed: loaded,
+			total: total
+		});
+
+		if (loaded >= total) {
+			reload();
+			cb2();
+		}
+	}
+	function checkLoaded(input, output) {
+		fse.copy(input, output)
+			.then(res => {
+				isEnd();
+			})
+			.catch(() => {
+				isEnd();
+			});
+	}
+	glob(`${config.src}**/*.{png,jpg,gif,ico,svg}`, (error, files) => {
+		total = files.length;
+		files.map((file, key) => {
+			let relativePath = file.replace(config.src, "");
+			let outPath = path.resolve(config.dist, path.dirname(Utli.toVersionUrl(relativePath)), path.basename(relativePath));
+			console.log(outPath);
+			console.log(relativePath);
+			var stat = fse.statSync(file);
 			var compressed = db
 				.get("images")
 				.find({ file: relativePath })
 				.value();
+
 			if (!stat || !stat.mtimeMs || !compressed || compressed.mtimeMs != stat.mtimeMs) {
 				tinypng
-					.compressImg(file.path, file.path)
+					.compressImg(file, file)
 					.then(res => {
 						if (!!res) {
-							stat = fse.statSync(file.path);
-							
+							stat = fse.statSync(file);
 							if (!compressed) {
 								db.get("images")
 									.push({ file: relativePath, mtimeMs: stat.mtimeMs || "" })
@@ -73,20 +75,18 @@ function images(cb2) {
 									.write();
 							}
 						}
-						checkLoaded();
+						checkLoaded(file, outPath);
 					})
 					.catch(err => {
-						checkLoaded();
+						checkLoaded(file, outPath);
 					});
 			} else {
-				checkLoaded();
+				checkLoaded(file, outPath);
 			}
-		})
-	);
-	setTimeout(function () {
-		if (total == 0) {
+		});
+		if (files.length == 0) {
 			cb2();
 		}
-	}, 1000);
+	});
 }
 module.exports = images;
--- gulpfile.js/modules/script/index.js
+++ gulpfile.js/modules/script/index.js
@@ -1,11 +1,9 @@
 const Pkg = require("./pkg");
-const Util = require("../../libs/util");
 const config = require("../../config");
-const through = require("through2");
-const { src } = require("gulp");
 const jsState = require("./state");
 const Copy = require("./copy");
 const watchjs = require("./watchjs");
+const glob = require("glob");
 async function Scripts(cb) {
 	// console.log('start scripts');
 	if (!global.ChangedJs) {
@@ -18,18 +16,17 @@ async function Scripts(cb) {
 		}
 		await Pkg(entries);
 		//先打包然后对其余的js进行复制
-		src(`${config.src}**/*.js`)
-			.pipe(
-				through.obj(function (file, enc, cb) {
-					let relpath = config.src + file.relative;
-					if (!jsState.getJs(relpath)) {
-						Copy(relpath);
-					}
-					cb(null, file);
-				})
-			)
-	}
-	else {
+		var promiseArr = [];
+		glob(`${config.src}**/*.js`, (error, files) =>{
+			for (var i in files) {
+				let relpath = files[i];
+				if (!jsState.getJs(relpath)) {
+					promiseArr.push(Copy(relpath));
+				}
+			}
+		});
+		await Promise.all(promiseArr);
+	} else {
 		//就是文件发生改变
 		for (var i in global.ChangedJs) {
 			console.log(global.ChangedJs[i] + "发生改变");
@@ -37,9 +34,8 @@ async function Scripts(cb) {
 			var jsProp = jsState.getJs(curfile);
 			//return;
 			if (!jsProp || (!jsProp.inHtml && !jsProp.inJs)) {
-				Copy(curfile);
-			}
-			else {
+				await Copy(curfile);
+			} else {
 				if (jsProp.inHtml) {
 					await Pkg([curfile]);
 				}
